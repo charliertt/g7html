812606ba4db015c6717deaa3171ec78f159ca6c7
charliertt <carlitosviveros588@gmail.com>
2024-11-13 18:20:33 -0500

    Puliendo detalles 2

--- JS/distMenu.js
+++ JS/distMenu.js
@@ -1,24 +1,18 @@
 function fmenu(page) {
+    
     fetch(`${page}.html`)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('cuerpoPag').innerHTML = data;
-                       
-                // Configurar Parsley validaciones de formulario
-                $('#registroForm').parsley({
-                    trigger: 'input'
-                });
-                // Para asegurar la validación inmediata mientras el usuario escribe
-                $('input').on('input', function () {
-                    $(this).parsley().validate();
-                });
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('cuerpoPag').innerHTML = data;
 
-                // Seleccionar el modal llamandolo por su Id
-               
-                var modal = new bootstrap.Modal(document.getElementById('exampleModal'), { backdrop: 'static', keyboard: false });
-                modal.show(); 
-
-        }).catch(error => console.log('Error al cargar la página', error));
+        // Inicializar Parsley después de cargar el formulario
+        $('#registroForm').parsley();
+        
+        // Configuración del modal
+        var modal = new bootstrap.Modal(document.getElementById('exampleModal'), { backdrop: 'static', keyboard: false });
+        modal.show();
+    })
+    .catch(error => console.log('Error al cargar la página', error));
 }
 
 //Funcion para mostrar la imagen
